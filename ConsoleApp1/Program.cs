cfd241bddd1352149aa63b7f6c9b7440f175bf65
John Doe <john.doe@gmail.com>
2019-07-10 10:22:40 -0300

    Estudando arrays simples

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello Git");
+            string[] nameList = new string[2] { "valor1", "valor2" };
+
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            //string[] dateList = new string[10];
+            string[] dateList = new string[10000];
+
+            DateTime firstTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++) {
+                //dateList[i] = DateTime.Now.ToString("yyyyMMddhhmmssfff");
+                dateList[i] = DateTime.Now.ToString("hh:mm:ss.fff");
+                Console.WriteLine(dateList[i]);
+                // Thread.Sleep(1000);
+            }
+            Console.WriteLine((DateTime.Now - firstTime).ToString());
+           
+            /*
+            foreach (var item in dateList)
+                Console.WriteLine(item);
+                */
+            Console.ReadKey();
         }
     }
 }
