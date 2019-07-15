2c93d734d50ffedf61071814dc3d65997df5c0a0
hbsis <hbsis@BLUE01>
2019-07-15 09:07:35 -0300

    Review Array bidimensional simples

---
+++ ListaBiDeManeiraSimples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaBiDeManeiraSimples {
+    class Program {
+        static void Main(string[] args) {
+            string[,] listaDeNome = new string[5, 2];
+
+            for (int i = 0; i < listaDeNome.GetLength(0); i++) {
+                listaDeNome[i, 0] = i.ToString();
+                listaDeNome[i, 1] = $"Henoc_{i}";
+
+            }
+            for (int i = 0; i < listaDeNome.GetLength(0); i++) {
+                Console.WriteLine($"ID:{listaDeNome[i, 0]} - Nome{listaDeNome[i, 1]}");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
