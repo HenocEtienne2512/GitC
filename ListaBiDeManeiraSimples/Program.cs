39ebdbea1e15a8fd842b7c08e59bca2b0bc68c3e
hbsis <hbsis@BLUE01>
2019-07-15 10:05:15 -0300

    Lista bidimensional de maneira simples

--- ListaBiDeManeiraSimples/Program.cs
+++ ListaBiDeManeiraSimples/Program.cs
@@ -8,17 +8,42 @@ namespace ListaBiDeManeiraSimples {
     class Program {
         static void Main(string[] args) {
             string[,] listaDeNome = new string[5, 2];
+            CarregarInformacoesElasEmTela(ref listaDeNome);
 
-            for (int i = 0; i < listaDeNome.GetLength(0); i++) {
-                listaDeNome[i, 0] = i.ToString();
-                listaDeNome[i, 1] = $"Henoc_{i}";
+            Console.ReadKey();
+
+            Console.WriteLine("Informe o ID do registro a ser pesquisado.");
+
+            PesquisandoInformacoesnaNossaLista(listaDeNome, Console.ReadLine());
+        }
+
+        public static void CarregarInformacoesElasEmTela(ref string[,] arrayBi) {
+         
+            for (int i = 0; i < arrayBi.GetLength(0); i++) {
+                arrayBi[i, 0] = i.ToString();
+                arrayBi[i, 1] = $"Henoc_{i}";
 
             }
-            for (int i = 0; i < listaDeNome.GetLength(0); i++) {
-                Console.WriteLine($"ID:{listaDeNome[i, 0]} - Nome{listaDeNome[i, 1]}");
+            for (int i = 0; i < arrayBi.GetLength(0); i++) {
+                Console.WriteLine($"ID:{arrayBi[i, 0]} - Nome{arrayBi[i, 1]}");
 
             }
             Console.ReadKey();
         }
+
+        public static void PesquisandoInformacoesnaNossaLista(string[,] arrayBi, string pId) {
+            for (int i = 0; i < arrayBi.GetLength(0); i++) {
+                if (arrayBi[i,0] == pId) {
+                    Console.WriteLine($"Informação escolhida: Id:{arrayBi[i,0]} - Nome: {arrayBi[i,1]}");
+
+                    return;
+                }
+            }
+            Console.WriteLine("Infelizmente a busca pelo  id não resulto em nenhuma informação");
+
+            Console.ReadKey();
+        }
+
     }
 }
+
