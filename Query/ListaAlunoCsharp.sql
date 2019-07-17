6d6427a3bc9c6f01071ddb12be1e453a1eacef81
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 13:28:33 -0300

    Lista aula C sharp

---
+++ Query/ListaAlunoCsharp.sql
@@ -0,0 +1,32 @@
+﻿update AulaCsharp
+set idade = '38' 
+where Id = 2;
+go
+select * from AulaCsharp;
+
+/*
+insert into AulaCsharp
+(Aluno,Idade,ativo,UsuInc,UsuAlt,DatInc,DatAlt)
+values
+('Christanie','62','1','006','006',GETDATE(),GETDATE());
+
+insert into AulaCsharp
+(Aluno,Idade,ativo,UsuInc,UsuAlt,DatInc,DatAlt)
+values
+('Jn Baptiste','30','1','007','007',GETDATE(),GETDATE());
+
+insert into AulaCsharp
+(Aluno,Idade,ativo,UsuInc,UsuAlt,DatInc,DatAlt)
+values
+('Patrice','70','1','003','003',GETDATE(),GETDATE());
+
+insert into AulaCsharp
+(Aluno,Idade,ativo,UsuInc,UsuAlt,DatInc,DatAlt)
+values
+('Stephania','32','1','004','004',GETDATE(),GETDATE());
+
+insert into AulaCsharp
+(Aluno,Idade,ativo,UsuInc,UsuAlt,DatInc,DatAlt)
+values
+('Martine','24','1','009','009',GETDATE(),GETDATE());
+*/
