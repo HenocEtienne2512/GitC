212f6ffacd1b376adbe5507e4a9a3d7bef4fca0d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:09:36 -0300

    SELECT FoodTruck

---
+++ Query/SELECT FoodTruck.sql
@@ -0,0 +1,14 @@
+﻿
+select
+	Tipo,
+
+	(SUM(TotalVenda) / count(*)) as 'Media',
+	Month(DataVenda) as 'Mês Venda'
+from FoodTruck
+	where Month(DataVenda) between 6 and 7
+	group by Tipo, Month(DataVenda)
+	order by 'Media'desc;
+
+
+select * from FoodTruck
+order by Tipo;
