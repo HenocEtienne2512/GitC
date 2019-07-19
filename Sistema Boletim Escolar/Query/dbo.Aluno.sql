fe711a9dd7ffcab90d9700be53284f73ee6246d1
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 16:59:38 -0300

    Select da tabela do aluno

---
+++ Sistema Boletim Escolar/Query/dbo.Aluno.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Aluno] (
+    [Id]    INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]  VARCHAR (50) NOT NULL,
+    [Turma] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Alunos_Turma] FOREIGN KEY ([Turma]) REFERENCES [dbo].[Turma] ([Id])
+);
+
---
+++ Sistema Boletim Escolar/Query/dbo.Boletim.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Boletim] (
+    [Id]         INT  IDENTITY (1, 1) NOT NULL,
+    [Nota]       INT  NULL,
+    [Frequencia] BIT  NULL,
+    [Data]       DATE NOT NULL,
+    [Aluno]      INT  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Boletim_Aluno] FOREIGN KEY ([Aluno]) REFERENCES [dbo].[Aluno] ([Id])
+);
+
