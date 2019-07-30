5d61d96dc77621f26d66f9fd2775f8800dd85ffe
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-30 16:45:18 -0300

    comite do dia

---
+++ CadastroAluno/CadastroAluno/Classes/Cadastro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Classes {
+    public class Cadastro {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime DatInc { get; set; } = DateTime.Now;
+
+    }
+}
