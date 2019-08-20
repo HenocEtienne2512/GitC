6ea7861a9acd736e988811e3d1f4a14df43eeecb
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-20 17:22:16 -0300

    commit do dia 20-08-2019

--- 12-08-2019 a 16-08-2019/WEBAPIRESTFULL/SITETESTAPIRESTFULL/js/generos.js
+++ 12-08-2019 a 16-08-2019/WEBAPIRESTFULL/SITETESTAPIRESTFULL/js/generos.js
@@ -1,123 +1,18 @@
 
-
-
-    /* Ao carregar o documento o mesmo inicia o conteudo desde script*/
+	/* Ao carregar o documento o mesmo inicia o conteudo desde script*/
     jQuery(document).ready(function(){
-		/* Indica que o evento submit do form irá realizar esta ação agora*/
-		jQuery('#formusuarios').submit(function(){
-			/* Neste contesto 'this' representa o form deste ID  #myform */                
-			var dados = $(this).serialize();
 
-			 var settings = {
-			  "crossDomain": true,
-			  "url": "http://localhost:59271/Api/Generos",
-			  "method": "POST",
-			  "headers": {
-				"Content-Type": "application/x-www-form-urlencoded",
-				"Accept": "*/*"
-			  },
-			  "data": dados
-			}
+		jQuery('#btnCancelar').click(function(){
 
-			$.ajax(settings).done(function (response) {
-			    GetMethod();
-			});
-			
-			return false;
-		});
-		
-		jQuery('#bntSalvar').click(function(){
-			 Editing();
-			 
-			$('#bntSubmit').show();
-			$('#bntSalvar').hide();
-			$('#bntCancelar').hide();
-			
 			$('#Id').val("");
-			$('#Nome').val("");
-			$('#Login').val("");
-			$('#Email').val("");
-			$('#Senha').val("");
-			$('#Ativo select').val("true");
+			$('#Tipo').val("");
+			$('#Descricao').val("");
 		});
-		
-		jQuery('#bntCancelar').click(function(){
-			$('#bntSubmit').show();
-			$('#bntSalvar').hide();
-			$('#bntCancelar').hide();
-			
-			$('#Id').val("");
-			$('#Nome').val("");
-			$('#Login').val("");
-			$('#Email').val("");
-			$('#Senha').val("");
-			$('#Ativo select').val("true");
-		});
-		
-		GetMethod();
+
+		GetMethod(null);
 	});
-	
-	function GetByID(id){
-        $('#bntSubmit').hide();
-		$('#bntSalvar').show();
-		$('#bntCancelar').show();
-		
-        var settings = {
-			"async": true,
-			"crossDomain": true,
-			"url": "http://localhost:59271/Api/Generos/"+id,
-			"method": "GET",
-				"headers": {
-					"Content-Type": "application/json",
-					"Accept": "*/*"
-				}
-			}
-	
-			$.ajax(settings).done(function (response) {
-				$('#Id').val(response.Id);
-				$('#Tipo').val(response.Tipo);
-				$('#Descricao').val(response.Descricao);
-			});
-		
-	}
-	
-	function Editing(){
-		var dados = $('#formusuarios').serialize();
-		var id = $('#Id').val();
-
-		 var settings = {
-		  "crossDomain": true,
-		  "url": "http://localhost:59271/Api/Generos/"+id,
-		  "method": "PUT",
-		  "headers": {
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Accept": "*/*"
-		  },
-		  "data": dados
-		}
-
-		$.ajax(settings).done(function (response) {
-		    GetMethod();
-		});
-	}
-	
-	function Deleting(id){
-			 var settings = {
-			  "crossDomain": true,
-			  "url": "http://localhost:59271/Api/Generos/"+id,
-			  "method": "DELETE",
-			  "headers": {
-				"Content-Type": "application/x-www-form-urlencoded",
-				"Accept": "*/*"
-			  }
-			}
-
-			$.ajax(settings).done(function (response) {
-			    GetMethod();
-			});
-	}
     
-    function GetMethod(){
+    function GetMethod(object){
 			var settings = {
 				"async": true,
 				"crossDomain": true,
@@ -130,43 +25,41 @@
 				}
 
 				$.ajax(settings).done(function (response) {
-				  RefrestGrid(response);
+				  RefreshGrid(response);
 				});
 			
 			return false;
     }
    
-    function RefrestGrid(contentValue){
+    function RefreshGrid(contentValue){
 	   $('#tDataGrid').empty();
 	   $('#tDataGrid').html(  '<tbody>'
 							+ 	'<tr>'
 							+ 		'<th>ID</th>'
 							+ 		'<th>Tipo</th>'
-							+ 		'<th>Descricao</th>'
+							+ 		'<th>Descrição</th>'
 							+ 		'<th>Opções</th>'
 							+ 	'</tr>'
 							+ '</tbody>');
 
 		$.each(contentValue,function(index,value) {
         var row =     '<tr>'
-						+ '<td>' + value.Id       + '</td>'
-						+ '<td>' + value.Tipo    + '</td>'
-						+ '<td>' + value.Descricao    + '</td>'					
-						+ '<td>' 
-						+ 	'<div    class=\'col-md-12\' style=\'float: right;\'>'
-						+ 		'<div    class=\'col-md-6\'>'
-						+ 			'<button class=\'btn btn-block btn-danger col-md-3 ajax\' type=\'button\'  onclick=\'Deleting('+ value.Id +')\'>Remover</button>'
-						+ 		'</div>'
-						+ 		'<div     class=\'col-md-6\'>'
-						+ 			'<button  class=\'btn btn-block btn-success col-md-3\'    type=\'button\'  onclick=\'GetByID('+ value.Id +')\'\>Editar</button>'
-						+ 		'</div>'
-						+ 	'</div>'
-						+ '</td>'
+					+ '<td>' + value.Id           + '</td>'
+					+ '<td>' + value.Tipo         + '</td>'
+					+ '<td>' + value.Descricao    + '</td>'
+					+ '<td>' 
+					+ 	'<div    class=\'col-md-12\' style=\'float: right;\'>'
+					+ 		'<div    class=\'col-md-6\'>'
+					+ 			'<button class=\'btn btn-block btn-danger col-md-3 btn-delet-event\' type=\'button\' send-post=\'Generos\'  value=\''+ value.Id +'\'>Remover</button>'
+					+ 		'</div>'
+					+ 		'<div     class=\'col-md-6\'>'
+					+ 			'<button  class=\'btn btn-block btn-success col-md-3 btn-editing-event\' send-post=\'Generos\' value=\''+ value.Id + '\' type=\'button\'\>Editar</button>'
+					+ 		'</div>'
+					+ 	'</div>'
+					+ '</td>'
 					+ '</tr>';
         $('#tDataGrid').append(row);
 		});
+
+		SetGridClickEvents();
     }
-	
-	
-  
-  
--- 12-08-2019 a 16-08-2019/WEBAPIRESTFULL/SITETESTAPIRESTFULL/js/usuarios.js
+++ 12-08-2019 a 16-08-2019/WEBAPIRESTFULL/SITETESTAPIRESTFULL/js/usuarios.js
@@ -1,9 +1,8 @@
 /* Ao carregar o documento o mesmo inicia o conteudo desde script*/
 jQuery(document).ready(function(){
 
-	jQuery('#bntCancelar').click(function(){
-		$('#bntCancelar').hide();
-		
+	jQuery('#btnCancelar').click(function(){
+
 		$('#Id').val("");
 		$('#Nome').val("");
 		$('#Login').val("");
@@ -14,37 +13,6 @@ jQuery(document).ready(function(){
 
 	GetMethod(null);
 });
-
-function GetByID(id){
-	//$('#bntSubmit').hide();
-	//$('#bntSalvar').show();
-	$('#bntCancelar').show();
-	
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "http://localhost:59271/Api/Usuarios/"+id,
-		"method": "GET",
-			"headers": {
-				"Content-Type": "application/json",
-				"Accept": "*/*"
-			}
-		}
-
-		$.ajax(settings).done(function (response) {
-			$('#Id').val(response.Id);
-			$('#Nome').val(response.Nome);
-			$('#Login').val(response.Login);
-			$('#Senha').val(response.Senha);
-			$('#Email').val(response.Email);
-			$('#Ativo select').val(response.Ativo);
-		});
-	
-}
-
-function Potator(object){
-	alert('Teste potator pulgmatica');
-}
 
 function GetMethod(object){
 		var settings = {
@@ -80,21 +48,21 @@ function RefreshGrid(contentValue){
 
 	$.each(contentValue,function(index,value) {
 	var row =     '<tr>'
-					+ '<td>' + value.Id       + '</td>'
-					+ '<td>' + value.Nome    + '</td>'
-					+ '<td>' + value.Login    + '</td>'
-					+ '<td>' + value.Email    + '</td>'
-					+ '<td>' + value.Ativo    + '</td>'
-					+ '<td>' 
-					+ 	'<div    class=\'col-md-12\' style=\'float: right;\'>'
-					+ 		'<div    class=\'col-md-6\'>'
-					+ 			'<button class=\'btn btn-block btn-danger col-md-3 btn-delet-event\' type=\'button\' send-post=\'Usuarios\'  value=\''+ value.Id +'\'>Remover</button>'
-					+ 		'</div>'
-					+ 		'<div     class=\'col-md-6\'>'
-					+ 			'<button  class=\'btn btn-block btn-success col-md-3\'    type=\'button\'  onclick=\'GetByID('+ value.Id +')\'\>Editar</button>'
-					+ 		'</div>'
-					+ 	'</div>'
-					+ '</td>'
+				+ '<td>' + value.Id       + '</td>'
+				+ '<td>' + value.Nome     + '</td>'
+				+ '<td>' + value.Login    + '</td>'
+				+ '<td>' + value.Email    + '</td>'
+				+ '<td>' + value.Ativo    + '</td>'
+				+ '<td>' 
+				+ 	'<div    class=\'col-md-12\' style=\'float: right;\'>'
+				+ 		'<div    class=\'col-md-6\'>'
+				+ 			'<button class=\'btn btn-block btn-danger col-md-3 btn-delet-event\' type=\'button\' send-post=\'Usuarios\'  value=\''+ value.Id +'\'>Remover</button>'
+				+ 		'</div>'
+				+ 		'<div     class=\'col-md-6\'>'
+				+ 			'<button  class=\'btn btn-block btn-success col-md-3 btn-editing-event\' send-post=\'Usuarios\' value=\''+ value.Id + '\' type=\'button\'\>Editar</button>'
+				+ 		'</div>'
+				+ 	'</div>'
+				+ '</td>'
 				+ '</tr>';
 	$('#tDataGrid').append(row);
 	});
